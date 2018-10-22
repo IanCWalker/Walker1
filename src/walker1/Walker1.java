f90cf1759c7d91be075dfbd2c95c033cfee0440a
icw2 <icw2@FSTU603003.pti.edu>
2018-10-22 17:46:56 -0400

    this is the first commit

---
+++ src/walker1/Walker1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package walker1;
+
+/**
+ *
+ * @author icw2
+ */
+public class Walker1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Walker1();
+    }
+    public static void Walker1(){
+        System.out.println("Ian Walker Again!!!");
+    }
+    
+}
